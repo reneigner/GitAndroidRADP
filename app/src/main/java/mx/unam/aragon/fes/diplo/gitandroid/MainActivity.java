4bf0e7a6edbc41342eea2523499930add9c00ad0
reneigner <ren.ico.0930@gmail.com>
2018-02-18 16:37:43 -0600

    Añadida funcionalidad Toast.

--- app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
+++ app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 txtVista.setText("Hola Github");
+                Toast.makeText(MainActivity.this,
+                        "Se cambio el mensaje en el TextView",Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -30,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 txtVista.setText("Diplomado Android");
+                Toast.makeText(MainActivity.this,
+                        "Se cambio el mensaje en el TextView",Toast.LENGTH_SHORT).show();
             }
         });
     }
